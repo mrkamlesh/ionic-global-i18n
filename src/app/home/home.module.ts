a6a994fbaa47b61698fcf9475d529722578d42d0
Kamlesh Kumar <kamlesh.kumar@agreeya.com>
2019-11-01 15:45:51 +0530

    first commit

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,13 +4,28 @@ import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { HttpClient } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
 import { HomePage } from './home.page';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, "./assets/i18n/", ".json");
+}
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    TranslateModule.forChild({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
     RouterModule.forChild([
       {
         path: '',
@@ -20,4 +35,4 @@ import { HomePage } from './home.page';
   ],
   declarations: [HomePage]
 })
-export class HomePageModule {}
+export class HomePageModule { }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Globalization } from '@ionic-native/globalization/ngx';
+import { TranslateService } from '@ngx-translate/core';
+
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,72 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public title: string;
+  public title_2: string;
+  public description: string;
+  public name: string;
+  public language: string;
+
+  constructor(private globalization: Globalization, private _translate: TranslateService) { }
+
+  ionViewDidEnter(): void {
+    console.log('challa?')
+    this.getDeviceLanguage()
+  }
+
+  _initialiseTranslation(): void {
+    this._translate.get('TITLE').subscribe((res: string) => {
+      console.log(res);
+      this.title = res;
+    });
+    this._translate.get('description').subscribe((res: string) => {
+      console.log(res);
+      this.description = res;
+    });
+    this._translate.get('TITLE_2', { value: 'John' }).subscribe((res: string) => {
+      console.log(res);
+      this.title_2 = res;
+    });
+    this._translate.get('data.name', { name_value: 'Marissa Mayer' }).subscribe((res: string) => {
+      console.log(res);
+      this.name = res;
+    });
+
+  }
+
+  public changeLanguage(): void {
+    this._translateLanguage();
+  }
+
+  _translateLanguage(): void {
+    console.log('language', this.language)
+    this._translate.use(this.language);
+    this._initialiseTranslation();
+  }
+
+  _initTranslate() {
+    // Set the default language for translation strings, and the current language.
+    this._translate.setDefaultLang('en');
+
+    if (this._translate.getBrowserLang() !== undefined) {
+      this.language = this._translate.getBrowserLang();
+      console.log('browser language is', this._translate.getBrowserLang());
+    }
+    else {
+      // Set your language here
+      this.language = 'en';
+    }
+
+    this._translateLanguage();
+  }
+
+  getDeviceLanguage() {
+    this.globalization.getPreferredLanguage()
+      .then(res => {
+        console.log(res)
+        this._initTranslate()
+      })
+      .catch(e => console.log(e));
+  }
 
 }
